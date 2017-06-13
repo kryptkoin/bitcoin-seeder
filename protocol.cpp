a60ae67687ea8838eb21641118c7246d289ca338
Peter Bushnell <bushsolo@gmail.com>
2017-06-13 15:16:54 +0100

    Convert to Kryptkoin

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xce, 0xfb, 0xfa, 0xdb };
 
 CMessageHeader::CMessageHeader()
 {
